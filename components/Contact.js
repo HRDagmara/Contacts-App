bc14b4538bd591de07068a34f890f25775c1ca0b
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-23 01:02:06 +0100

    Add styles.css

---
+++ components/Contact.js
@@ -0,0 +1,22 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  });
+  
